6d99d108ffdfa7aa9c8fdb164a563a4cfd953c00
woland7 <antonio.deiasio@gmail.com>
2020-06-02 11:58:14 +0200

    fix

--- src/main/java/it/unisannio/controller/AccountController.java
+++ src/main/java/it/unisannio/controller/AccountController.java
@@ -5,19 +5,12 @@ import it.unisannio.service.BranchLocal;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.transaction.UserTransaction;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 import java.net.URI;
-import java.util.List;
 
 @Consumes("text/plain")
 @Produces("text/plain")
